02f5dd7063449fad417f53523e506c3e56362f1f
Halil İbrahim LÜY <halilibrahim.luy@tbmm.gov.tr>
2021-03-10 17:53:29 +0300

    1

--- Deza.Models/Dtos/ReportDto.cs
+++ Deza.Models/Dtos/ReportDto.cs
@@ -1,7 +1,4 @@
 ﻿using Deza.Stone;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Deza.Models.Dtos
 {
--- Deza.MvcWebUI/Helper/ClientHelper.cs
+++ Deza.MvcWebUI/Helper/ClientHelper.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+﻿using Deza.Models.Dtos;
 using Deza.Stone;
-using System.Threading.Tasks;
-using Deza.Models.Dtos;
+using System.Collections.Generic;
+using System.Net.Http;
 
 namespace Deza.MvcWebUI.Helper
 {
